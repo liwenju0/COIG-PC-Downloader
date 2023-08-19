6cfa4ff1387f4a017ba0e245bd01e646c46f4b42
wenju.li <wenju.li@deepctr.cn>
2023-08-19 11:59:25 +0800

    init

---
+++ downloader.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Function to download a file
+download_file() {
+   url="$1"
+   file_name="$(echo "$url" | sed 's/.*\///')"
+   if [ ! -f "data/${file_name}" ]; then
+        echo "Downloading ${file_name}..."
+        while ! curl -q --header "Cookie: token=fsxbwUDUcEHfDEcXbhDimvFoAtcOGmJOBNUxWtbnBSIjNyUhLWBkdFOCmyVrouCUOVtTxTcnfNoUsKlbgktWgyXdsJNevJmykEreJcVrmlLnGFfErVRImrdISbSDwdxD" "$url" -o "data/${file_name}" -L; do
+            sleep 1
+        done
+        echo "${file_name} Download completed"
+   fi
+}
+
+# Read the txt file line by line
+while read -r line; do
+   # Check if the line is not empty
+   if [ -n "$line" ]; then
+       # Download the file in a separate process
+       download_file "$line" &
+   fi
+done < data_links.txt
+
+# Wait for all downloads to complete
+wait
+
+echo "All downloads are complete."
